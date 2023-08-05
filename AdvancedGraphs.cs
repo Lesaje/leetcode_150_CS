2b89519a8ae893a0fd4460775b4479e0d4964d3b
Oleg Chaika <olehchaika.prop@gmail.com>
2023-08-05 17:59:06 +0300

    Kruskal-Prim algorithm

--- AdvancedGraphs.cs
+++ AdvancedGraphs.cs
@@ -1,9 +1,46 @@
 namespace Leetcode150;
 
-public class AdvancedGraphs
+public static class AdvancedGraphs
 {
-    public int MinCostConnectPoints(int[][] points)
+    public static int ManhattanDistance((int, int) a, (int, int) b)
     {
-        return 0;
+        return Math.Abs(a.Item1 - b.Item1) + Math.Abs(a.Item2 - b.Item2);
+    }
+
+    record Edge(int From, int To);
+    
+    //not quite working
+    public static int MinCostConnectPoints(int[][] points)
+    {
+        var connectedVertices = new Dictionary<int, int>();
+        var edges = new PriorityQueue<Edge, int>();
+        var answer = 0;
+
+        connectedVertices.Add(0, 0);    //first vertex
+        for (var i = 1; i < points.Length; i++)
+        {
+            edges.Enqueue(new Edge(0, i), ManhattanDistance((points[0][0], points[0][1]), (points[i][0], points[i][1])));
+        }
+        
+        while (connectedVertices.Count != points.Length)
+        {
+            var distance = 0;
+            var nextEdge = new Edge(0, 0);
+            edges.TryDequeue(out nextEdge, out distance);
+            answer += distance;
+            connectedVertices.Add(nextEdge.To, nextEdge.To);
+            for (var i = 0; i < points.Length; i++)
+            {
+                if (!connectedVertices.ContainsKey(i))
+                {
+                    edges.Enqueue(
+                        new Edge(nextEdge.To, i), 
+                        ManhattanDistance((points[nextEdge.To][0], points[nextEdge.To][1]), (points[i][0], points[i][1]))
+                        );
+                }
+            }
+        }
+
+        return answer;
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@
 
 int[][] arr = new int[][]
 {
-    new int[] { 1, 5 },
+    new int[] { 0, 0 },
+    new int[] { 2, 2 },
+    new int[] { 3, 10 },
+    new int[] { 5, 2 },
+    new int[] { 7, 0 }
 };
 
-Console.WriteLine(Greedy.Jump(new int[] {1, 2, 3}));
+Console.WriteLine(AdvancedGraphs.MinCostConnectPoints(arr));
